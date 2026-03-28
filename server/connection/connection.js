01ef9807da72dcb1ef9f9a0f7391c9987de659f3
anshul-999 <anshul.b100@gmail.com>
2026-03-28 22:44:08 +0530

    fix ES module error in conn()

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const conn = async()=>{
-try{
-   const connDB = await mongoose.connect(process.env.MONGODB_STR);
-    console.log(`MongoDB Connected: ${connDB.connection.host}`)
-}catch(err){
-     console.error("Database connection error:", err);
+dotenv.config();
+
+const conn = async () => {
+  try {
+    const connDB = await mongoose.connect(process.env.MONGODB_STR);
+    console.log(`MongoDB Connected: ${connDB.connection.host}`);
+  } catch (err) {
+    console.error("Database connection error:", err);
     process.exit(1);
-}
-}
-module.exports= conn
+  }
+};
+
+export default conn;
