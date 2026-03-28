022744577a966dcbd0dd18c5c4d71de46322e8ff
anshul-999 <anshul.b100@gmail.com>
2026-03-28 22:48:13 +0530

    change file path - index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", authMiddleware, getProfileRoute);
 app.use("/api/jobs", jobRoutes);
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Smart Job Hunter API");
 });
 
